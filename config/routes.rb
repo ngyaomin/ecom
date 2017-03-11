2819afd78b88afcf95e02d6cf2be12eebe68455c
Unknown <ngyaomin@ymail.com>
2017-03-11 11:22:10 +0800

    Set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :orders
-  resources :products
+
+  resources :products do
+    resources :orders
+  end
+
+  root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
