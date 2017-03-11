67e37286c0d2d8769c515418f195e129a95b0970
Unknown <ngyaomin@ymail.com>
2017-03-11 20:36:29 +0800

    Add user id to product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170311123416) do
+ActiveRecord::Schema.define(version: 20170311123605) do
 
   create_table "orders", force: :cascade do |t|
     t.integer  "product_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20170311123416) do
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
     t.string   "product_image"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
