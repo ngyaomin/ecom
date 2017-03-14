20129f8b4e35954ad355109701f684cb8e66b068
Unknown <ngyaomin@ymail.com>
2017-03-14 11:10:41 +0800

    testing

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  context "associations" do
+    should belong_to(:user)
+  end
+
+  context "validations" do
+    should validate_presence_of(:product_image)
+    should validate_presence_of(:name)
+    should validate_presence_of(:price)
+    should validate_presence_of(:description)
+    should validate_presence_of(:inventory_available)
+    should validate_presence_of(:purchase_quantity)
+  
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,12 @@ class UserTest < ActiveSupport::TestCase
   context "associations" do
     should have_many(:products)
   end
+
+  context "validations" do
+    should validate_presence_of(:email)
+    should validate_uniqueness_of(:email)
+    should allow_value("user@example.com").for(:email)
+    should_not allow_value("not-an-email").for(:email)
+  end
+
 end
