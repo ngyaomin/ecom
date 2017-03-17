dee03a440ea2d6dfabcb0dbdda56c15193ef2168
Unknown <ngyaomin@ymail.com>
2017-03-17 16:48:17 +0800

    Refactoring

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,14 @@
 module ProductsHelper
+
+  def check_discounted_price(product)
+
+    if user_signed_in?
+      product.price = product.price*ENV["discount_rate"].to_f
+      number_to_currency product.price
+    else
+      number_to_currency product.price
+    end
+
+  end
+
 end
