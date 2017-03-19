48b25f2e1021e2dffc4001a313f100da462c8407
Unknown <ngyaomin@ymail.com>
2017-03-19 21:04:40 +0800

    Better UI

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,5 +1,15 @@
 module CartsHelper
+
   def check_price(line_item)
     number_to_currency line_item.product.price
   end
+
+  def check_status(line_item)
+    if user_signed_in?
+      return "Subtotal (including discounts)"
+    else
+      return "Subtotal (Sign up or log in to enjoy 15% off) "
+    end
+  end
+
 end
