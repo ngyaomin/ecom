caa11526d2a1836c29e8ce761e8ee99a8678e989
Unknown <ngyaomin@ymail.com>
2017-03-19 20:53:42 +0800

    debug

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,11 +1,5 @@
 module CartsHelper
   def check_price(line_item)
-    if user_signed_in?
-      line_item.product.price = line_item.product.price*ENV["discount_rate"].to_f
-      number_to_currency line_item.product.price
-    else
-      number_to_currency line_item.product.price
-    end
-
+    number_to_currency line_item.product.price
   end
 end
